83869be5f1d97a5b4168b0fd785e3ca9a6f44350
Sydney McGee <mcgee.sydney@gmail.com>
2022-06-11 13:31:40 -0500

    Completed problem 7

--- school_db/views.py
+++ school_db/views.py
@@ -351,7 +351,8 @@ LIMIT 21
 def problem_seven(request):
 
     # Make sure to set this equal to the primary key of the row you just created!
-    student_id = 11
+    student_id = 12 # Ran the previous problem twice.  So I'm deleting 12 here.
+    Student.objects.filter(pk=student_id).delete()
 
 
     try:
